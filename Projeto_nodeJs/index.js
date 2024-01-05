46f5cb434355dd920515c5678a1024ae619d8520
Vitor Silva <vitormarcos2005@hotmail.com>
2024-01-05 18:11:15 -0300

    Adicionando as funções ReadWorkSheet e AddUser

--- Projeto_nodeJs/index.js
+++ Projeto_nodeJs/index.js
@@ -19,3 +19,24 @@ async function GetDoc() {
     return doc;
 }
 
+async function ReadWorkSheet() {
+    let sheet = (await GetDoc()).doc.sheetsByIndex[0]
+    let rows = await sheet.getRows()
+    let users = rows.map(row =>{
+        return row.toObject()
+    })
+    return users
+}
+
+async function AddUser(data = {}) {
+    const response = await fetch('https://apigenerator.dronahq.com/api/OIh6uZKu/usuarios', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return response.json()
+}
+
+    
