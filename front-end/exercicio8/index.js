69dc79231a2f18572cad0204df22f258e490a3bb
Vitor Silva <vitormarcos2005@hotmail.com>
2024-03-07 20:38:22 -0300

    Pegando o valor dos campos do formulário e mostrando no console

---
+++ front-end/exercicio8/index.js
@@ -0,0 +1,35 @@
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    const inputNome = nome.value
+
+    console.log(inputNome)
+})
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    const inputEndereco = endereco.value
+
+    console.log(inputEndereco)
+})
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    const inputFuncionamento = dias.value
+
+    console.log(inputFuncionamento)
+})
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    const inputAbertura = abertura.value
+
+    console.log(inputAbertura)
+})
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    const inputFechamento = fechamento.value
+
+    console.log(inputFechamento)
+})
+
